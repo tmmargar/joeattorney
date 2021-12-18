e63017975dce68165f0f2e162a50490e67978ad5
Todd Margarita <todd@toddandpatti.com>
2021-12-17 21:47:10 -0500

    Default baseline

---
+++ Joe/scripts/common.js
@@ -0,0 +1,35 @@
+function buildFooter() {
+  const val = "<a href=\"index.html\">Home</a> | <a href=\"aboutUs.html\">About Us</a> | <a href=\"whyChoose.html\">Why Choose JPV</a> | <a href=\"consultServices.html\">Consulting Services</a> | <a href=\"legalServices.html\">Legal Services</a> | <a href=\"disclaimer.html\">Disclaimer</a> | <a href=\"contactUs.html\">Contact Us</a><br /><br />All Content &copy;2021</td>";
+  return val;
+}
+function buildNavigation() {
+  const val = 
+    " <ul>\r\n" +
+    "  <li><a href=\"index.html\">Home</a></li>\r\n" +
+    "  <li><a href=\"aboutUs.html\">About Us</a></li>\r\n" +
+    "  <li><a href=\"whyChoose.html\">Why Choose JPV</a></li>\r\n" +
+    "  <li><a href=\"consultServices.html\">Consulting Services</a>\r\n" +
+    "   <ul>\r\n" +
+    "    <li><a href=\"consultServices_revenueEnhance.html\">Revenue Enhancements</a></li>\r\n" +
+    "    <li><a href=\"consultServices_denials.html\">Denials Reductions &amp; Commercial Litigation</a></li>\r\n" +
+    "    <li><a href=\"consultServices_interimMgt.html\">Interim Management Support</a></li>\r\n" +
+    "    <li><a href=\"consultServices_measurement.html\">Measurement</a></li>\r\n" +
+    "    <li><a href=\"consultServices_efficiency.html\">Efficiency Improvement</a></li>\r\n" +
+    "    <li><a href=\"consultServices_seminars.html\">Seminars, Education, Retraining</a></li>\r\n" +
+    "   </ul>\r\n" +
+    "  </li>\r\n" +
+    "  <li><a href=\"legalServices.html\">Legal Services</a>\r\n" +
+    "   <ul>\r\n" +
+    "    <li><a href=\"legalServices_injury.html\">Injury Litigation</a></li>\r\n" +
+    "    <li><a href=\"legalServices_acctReceiv.html\">Account Receivable Collections</a></li>\r\n" +
+    "    <li><a href=\"legalServices_contractClaims.html\">Contract Claims</a></li>\r\n" +
+    "    <li><a href=\"legalServices_immigration.html\">Immigration</a></li>\r\n" +
+    "    <li><a href=\"legalServices_other.html\">Other Legal Claims Handled</a></li>\r\n" +
+    "   </ul>\r\n" +
+    "  </li>\r\n" +
+    "  <li><a href=\"disclaimer.html\">Disclaimer</a></li>\r\n" +
+    "  <li><a href=\"contactUs.html\">Contact Us</a></li>\r\n" +
+    " </ul>\r\n" +
+    "</div>";
+  return val;
+}
